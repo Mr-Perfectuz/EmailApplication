16a6b54a387779976746be7b08427b5d33b132c5
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-04-04 13:12:47 +0900

    EmailInfo has been implemented

--- src/EmailApp/EmailApp.java
+++ src/EmailApp/EmailApp.java
@@ -4,7 +4,6 @@ public class EmailApp {
     public static void main(String[] args){
         Email email = new Email("John", "Smith");
 
-        email.setAlternateEmail("js@gmail.com");
-        System.out.println(email.getAlternateEmail());
+       email.showInfo();
     }
 }
